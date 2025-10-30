5dd564a9e58c00026cbf52c8d02d87b898e50e71
Brainmush <joeplace913@gmail.com>
2025-10-30 17:55:22 -0400

    Commit

--- WhackAMolePrototype/Assets/ScorePopup.cs
+++ WhackAMolePrototype/Assets/ScorePopup.cs
@@ -4,35 +4,24 @@ public class ScorePopup : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public string stringText;
-    public Transform target;
+    //public Transform target;
     private Vector3 endPosition;
     private Vector3 startPosition;
 
     public float desiredDuration = 3f;
-    private float elapsedTime;
+    public float elapsedTime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    void Start()
     {
-        startPosition = transform.position;
-        endPosition = transform.position + Vector3.up;
         SetStringText(stringText);
     }
 
     // Update is called once per frame
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        float percentageComplete = elapsedTime / desiredDuration;
-        ////rotate to face the center
-        transform.position = Vector3.Lerp(startPosition, endPosition, percentageComplete);
-
-        //transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        transform.up = target.position - transform.position;
-        if (percentageComplete > 99)
-        {
-            Destroy(this);
-        }
+        Destroy(gameObject,desiredDuration);
+       
     }
 
     public void SetStringText(string text)

3f44f0236c30a860d3f0d7e01ea26a70f0db27cb
Brainmush <joeplace913@gmail.com>
2025-10-30 14:17:04 -0400

    commiting file change

---
+++ WhackAMolePrototype/Assets/Scripts/ExponentialValueOverTime.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+public class ExponentialValueOverTime : MonoBehaviour
+{
+    public AnimationCurve scalingOverTime;
+
+    public float scalingValue = 1f;
+    private float currentTime;
+    public List<float> minuteMilestones;
+
+    public float initialValue = 1f;
+    public float growthRate = 1.1f;
+    private float currentValue;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentTime = 0;
+        currentValue = initialValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentValue = initialValue * Mathf.Pow(growthRate, Time.time);
+        print("current value" + currentValue);
+
+        //issue is animation curve is a value between 0 and 1
+        //I want to normalize our scaling ot reach max scaling after 
+        currentTime += Time.deltaTime / minuteMilestones[minuteMilestones.Count -1];
+        scalingValue *= scalingOverTime.Evaluate(currentTime);
+        foreach (float milestone in minuteMilestones)
+        {
+            if (currentTime < milestone)
+            {
+                print(milestone);
+            }
+        }
+    }
+}
